f4751cdf834399a6fcc18a4f61e13cf6176767ed
@ilias_daniel <iliasdaniel1403@gmail.com>
2023-11-26 19:43:40 +0800

    up

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('/home');
+    return view('/auth/login');
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//welcome
+Route::get('/welcome', [App\Http\Controllers\HomeController::class, 'welcome'])->name('welcome');
+
+//login
+Route::get('/login', [App\Http\Controllers\Auth\LoginController::class, 'login'])->name('login');
+
+//register
+Route::get('/register', [App\Http\Controllers\Auth\RegisterController::class, 'register'])->name('register');
+
+//logout
+Route::get('/logout', [App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
